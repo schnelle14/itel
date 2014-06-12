5c30a51628bbc485b2d8a8dfb1c5f6254ada0c4f
Brando Sarmiento <brandovsarmiento@gmail.com>
2014-06-12 22:00:12 +1200

    remove old page types

--- mysite/code/pages/HomePage.php
+++ mysite/code/pages/HomePage.php
@@ -1,39 +1,28 @@
 <?php
 class HomePage extends Page {
 	static $db = array(
-			'Header_Left' => 'Text',
-			'Text_Left' => 'Text',
-			'Header_Right' => 'Text',
-			'Text_Right' => 'Text'
+			'Header_Text' => 'Text',
+			'Detail_Text' => 'Text',
+			'Mobile' => 'Text',
+			'Landline' => 'Text',
+			'Email' => 'Text'
 	);
 	static $has_one = array(
-			'Home_HeroImage' => 'Image',
-			'Logo_Image'     => 'Image',
-			'SmallDefault_HeroImage' => 'Image'
 	);
 	 	
 	public function getCMSFields() {
 		$fields = parent::getCMSFields();
-		$fields->removeByName('Small_HeroImage');
 		 
-		$fields->addFieldToTab('Root.Content.Main', new TextField('Header_Left', 'Left Header'), 'Content');
-		$fields->addFieldToTab('Root.Content.Main', new TextAreaField('Text_Left', 'Left Text'), 'Content');
-		$fields->addFieldToTab('Root.Content.Main', new TextField('Header_Right', 'Right Header'), 'Content');
-		$fields->addFieldToTab('Root.Content.Main', new TextAreaField('Text_Right', 'Right Text'), 'Content');
+		$fields->addFieldToTab('Root.Content.Home', new TextField('Header_Text', 'Header Text'));
+		$fields->addFieldToTab('Root.Content.Home', new TextAreaField('Detail_Text', 'Detail Text'));
 		
-		$homeHeroImage = new UploadField('Home_HeroImage', 'Please upload home page hero image'); 
-		$homeHeroImage->getValidator()->setAllowedExtensions(array('jpg', 'jpeg', 'png', 'gif')); 
-		$homeHeroImage->setConfig('allowedMaxFileNumber', 1);
-		$fields->addFieldToTab("Root.Content.Images", $homeHeroImage);
-// 		$fields->addFieldToTab("Root.Content.Images", new ImageField('Logo_Image', 'Company Logo (200 x 100)'));
-// 		$fields->addFieldToTab("Root.Content.Images", new ImageField('SmallDefault_HeroImage', 'Default Hero Image for other pages (1600 x 230)', null, null, null, 'HeroImages'));
-
+		$fields->addFieldToTab('Root.Content.Contact', new TextField('Mobile', 'Mobile No.'));
+		$fields->addFieldToTab('Root.Content.Contact', new TextField('Landline', 'Landline No.'));
+		$fields->addFieldToTab('Root.Content.Contact', new TextField('Email', 'Email Address'));
 		
 		return $fields;
 	}
 }
 class HomePage_Controller extends Page_Controller {
-	public function getLogo(){
-		return $this->Logo_Image;
-	}
+	// home page
 }
